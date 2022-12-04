48ca1f6d9fbcb03ebd7b4e0df51028f72d737bbf
Jen-Chieh Shen <jcs090218@gmail.com>
2022-12-04 23:51:43 +0800

    Use binaries instead of npm

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,50 @@ import * as exec from "@actions/exec";
 import * as io from "@actions/io";
 import * as tc from "@actions/tool-cache";
 import fs from 'fs';
+const https = require('https');
+
+function getPlatform(): string {
+    switch (process.platform) {
+        case 'linux': return 'linux';
+        case 'darwin': return 'macos';
+        case 'win32': return 'win';
+    }
+    return 'linux';  /* Default: linux */
+}
+
+async function getLatest(): string {
+    let body = "";
+    await https.get(
+        'https://api.github.com/repos/emacs-eask/cli/tags',
+        (res) => {
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+            res.on("end", () => {
+                try {
+                    let json = JSON.parse(body);
+                } catch (error) {
+                    console.error(error.message);
+                };
+            });
+        });
+}
 
 async function run(): Promise<void> {
     try {
         const PATH = process.env.PATH;
 
-        const version = core.getInput("version");
+        const latestVersion = '0.7.2';
+        const inputVersion = core.getInput("version");
+        const version = inputVersion == 'snapshot' ? latestVersion : inputVersion;
+        const architecture = core.getInput("architecture");
+        const platform = getPlatform();
 
-        let cmd = "npm install -g @emacs-eask/cli";
+        const binUrl = `https://github.com/emacs-eask/cli/releases/download/${version}/eask_${version}_${platform}-${architecture}.zip`;
 
         core.startGroup("Installing Eask");
 
-        if (version != "snapshot") {
-            cmd += "@" + version;
-        }
 
-        await exec.exec(cmd);
 
         core.endGroup();
 
